bb0461dc7fb801087930241cf9351f5417004a12
Pedro Ha <ph@educreational.com>
2015-07-15 17:09:53 -0400

    add image for process

---
+++ camera.js
@@ -0,0 +1,26 @@
+  var initCamera = function(videoId) {
+    video = document.getElementById(videoId);
+    navigator.getMedia = ( navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia);
+
+    navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          video.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+        }
+        video.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );  
+  };
